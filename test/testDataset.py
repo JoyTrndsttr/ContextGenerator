1f77d483410a12790e6fd8bfc1ab927cd89fdd0f
JoyTrndsttr <7742947+JoyTrndsttr@user.noreply.gitee.com>
2024-12-02 23:05:38 +0800

    小修改

--- test/testDataset.py
+++ test/testDataset.py
@@ -5,7 +5,7 @@ import requests
 import os
 import json
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+# from requests.packages.urllib3.util.retry import Retry
 
 # 数据库连接配置
 db_config = {

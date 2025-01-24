ad5c057c52320811f0f903fc8beb3086639e4bff
Trndsttr <923674667@qq.com>
2025-01-24 02:34:04 +0000

    细节修改

--- data_analyse_ablation.py
+++ data_analyse_ablation.py
@@ -3,36 +3,53 @@ import model
 
 # with open('/mnt/ssd2/wangke/CR_data/dataset/cacr_python_with_llama_cr_new_trim4.json', 'r') as f:
 # with open('/mnt/ssd2/wangke/CR_data/dataset/cacr_python_with_llama_cr_new_trim2.json', 'r') as f:
-with open('/mnt/ssd2/wangke/CR_data/dataset/dataset_all_3.json', 'r') as f:
+with open('/mnt/ssd2/wangke/CR_data/dataset/dataset_part.json', 'r') as f:
     records = json.load(f)
 
-    score = [[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0],],[0,[0,0,0,0]],[0,[0,0,0,0]]]
-    score2 = [0,0,0,0]
+    # score = [[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0]],[0,[0,0,0,0],],[0,[0,0,0,0]],[0,[0,0,0,0]]]
+    score = []
+    for i in range(6):
+        turn_result = []
+        turn_result.append(0)
+        for i in range(8):
+            turn_result.append([0,0,0,0])
+        score.append(turn_result)
     abort_analysis_result = {
         "case1:Unable to get the prompt_for_instruction result_json": 0,
         "case2:Unable to extract function name from the prompt_for_instruction result using regular expressions": 0,
         "case3:The function name has already existed":0,
         "case4:Unable to find the definition of the function name":0
     }
+    ablation_info = [
+        "Summary_cut_precise",
+        "Summary_uncut_precise",
+        "Code_cut_precise",
+        "Code_uncut_precise",
+        "Summary_cut_default",
+        "Summary_uncut_default",
+        "Code_cut_default",
+        "Code_uncut_default"
+    ]
     num = 0
     ids = []
     for record in records:
-        #方便查看结果
-        record["code_diff"] = "Omit"
-        record["context"] = "Omit"
+        
         
         results = record['results']
         # if not record["_id"] > 0 : continue
         # if record["_id"] > 0 : continue
-        if record["_id"] < -2312 or record["_id"] > 0: continue
         if len(results) == 0: continue
-        score2[0] += record["gpt_bleu"]
-        score2[1] += record["gpt_bleu_trim"]
-        score2[2] += record["llama_bleu"]
-        score2[3] += record["llama_bleu_trim"]
 
         turn = -1
-        
+
+        new_code_flag = True
+        for result in results:
+            for ablation_result in result.get("ablation_results", []):
+                if not ablation_result.get("new_code", None) and not ablation_result.get("new_code_clipped", None):
+                    new_code_flag = False
+        if not new_code_flag: 
+            print(f"id:{record['_id']};new_code_flag:{new_code_flag}")
+            continue
 
         # if len(results) == 1: continue
         for result in results:
@@ -43,24 +60,29 @@ with open('/mnt/ssd2/wangke/CR_data/dataset/dataset_all_3.json', 'r') as f:
             if abort_analysis:
                 abort_analysis_result[abort_analysis] += 1
             
-            if not result.get("new_code"): continue
+            # if not result.get("new_code"): continue
             turn = result['turn'] - 1
-            if True:
-            # if result["bleu_trim"] > 50: 
-                score[turn][0] += 1
-                score[turn][1][0] += result["em"]
-                score[turn][1][1] += result["em_trim"]
-                score[turn][1][2] += result["bleu"]
-                score[turn][1][3] += result["bleu_trim"]
             
-            if turn == 1 and result["bleu_trim"] < results[0]["bleu_trim"]:
-                num += 1
-                if results[0]["bleu_trim"] - result["bleu_trim"] > 30:
-                    ids.append(record["_id"])
+            score[turn][0] += 1
+            ablation_results_len = len(result.get("ablation_results", []))
+            for i, ablation_result in enumerate(result.get("ablation_results", [])):
+                if True:
+                # if result["bleu_trim"] > 50: 
+                    score[turn][i+1][0] += result["ablation_results"][i]["em"]
+                    score[turn][i+1][1] += result["ablation_results"][i]["em_trim"]
+                    score[turn][i+1][2] += result["ablation_results"][i]["bleu"]
+                    score[turn][i+1][3] += result["ablation_results"][i]["bleu_trim"]
             
-            # if results[0]["bleu_trim"] > 50 and result["bleu_trim"] < 50:
-            if results[0]["bleu_trim"] - result["bleu_trim"] > 30:
-                print(f"id:{record['_id']};turn:{turn+1};bleu_trim:{result['bleu_trim']}")
+            # if turn == 1 and result["bleu_trim"] < results[0]["bleu_trim"]:
+            #     num += 1
+            #     if results[0]["bleu_trim"] - result["bleu_trim"] > 30:
+            #         ids.append(record["_id"])
+            
+            # # if results[0]["bleu_trim"] > 50 and result["bleu_trim"] < 50:
+            # if results[0]["bleu_trim"] - result["bleu_trim"] > 30:
+            #     print(f"id:{record['_id']};turn:{turn+1};bleu_trim:{result['bleu_trim']}")
+
+            if result["ablation_results"][1]["em"] == 1 and result["ablation_results"][4]["em"] != 1: print(f"{record['_id']}")
 
             # if result_json.find("need more information?\": \"False") != -1:
             #     break
@@ -69,19 +91,22 @@ with open('/mnt/ssd2/wangke/CR_data/dataset/dataset_all_3.json', 'r') as f:
         if turn == -1: continue
         # result = results[-1]
         # for i in range(len(results), 6):
-        result = results[turn]
-        for i in range(turn+1, 6):
-            # score[i][0] += 1
-            score[i][1][0] += result["em"]
-            score[i][1][1] += result["em_trim"]
-            score[i][1][2] += result["bleu"]
-            score[i][1][3] += result["bleu_trim"]
+        for j in range(8):
+            result = results[turn]
+            for i in range(turn+1, 6):
+                # score[i][0] += 1
+                score[i][j+1][0] += result["ablation_results"][j]["em"]
+                score[i][j+1][1] += result["ablation_results"][j]["em_trim"]
+                score[i][j+1][2] += result["ablation_results"][j]["bleu"]
+                score[i][j+1][3] += result["ablation_results"][j]["bleu_trim"]
 
-    print(f"num:{num}")    
+    # print(f"num:{num}")    
     for i in range(6):
         count = score[0][0]
         if count != 0:
-            print(f"turn:{i+1}; count:{score[i][0]}; em:{score[i][1][0]/count}; em_trim:{score[i][1][1]/count};  bleu:{score[i][1][2]/count}; bleu_trim:{score[i][1][3]/count}")
+            print(f"turn:{i+1};")
+            for j in range(8):
+                print(f"{ablation_info[j]}; count:{score[i][0]}; em:{score[i][j+1][0]/count}; em_trim:{score[i][j+1][1]/count};  bleu:{score[i][j+1][2]/count}; bleu_trim:{score[i][j+1][3]/count}")
     count = len(records)
 
     for key, value in abort_analysis_result.items():
